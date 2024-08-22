0d45dc3b2638e87fd7aee0ea9c6430f5da22d3de
m0nT3cR1s70 <mizar0.30@gmail.com>
2024-08-22 09:31:19 -0600

    Add files via upload

--- Unidad_1/sumaVectorial.cu
+++ Unidad_1/sumaVectorial.cu
@@ -1,7 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
-#define N 90000000
+#define N 2048
+
+
+double cpuTime(){
+  struct timeval tp;
+  gettimeofday(&tp,NULL);
+  return ((double)tp.tv_sec + (double)tp.tv_usec*1.e-06);
+}
+
+
+void compareResults(float *res_GPU, float *res_CPU, int size){
+  double epsilon = 1.0E-8;
+  int flag = 1;
+  for(int i=0; i<size; i++){
+    if(abs(res_GPU[i]-res_CPU[i])>epsilon){
+      printf("ERROR: suma distinta\n");
+      flag = 0;
+      break;
+    }
+  }
+  if(flag == 1){
+    printf("SUMA CORRECTA\n");
+  }
+}
+
+
+
+
 
 void initialData(float *A, int size){
   srand(0);
@@ -25,15 +53,18 @@ void showVector(float *A, int size){
 
 
 __global__ void sumaOnGPU(float *dA, float *dB, float *dC, int size){
-  int idx = threadIdx.x;
+  //int idx = threadIdx.x;
+  //int idx = blockIdx.x;
+  int idx = threadIdx.x + (blockIdx.x * blockDim.x);
   dC[idx] = dA[idx]+dB[idx];
 }
 
 
 
 
-
 int main(){
+  // Variables para medir tiempo 
+  double tic,toc, tictocS = 0, tictocP = 0;
   // Definimos vector
   size_t nBytes = N * sizeof(float);
   float *h_A, *h_B, *h_C,*h_res;
@@ -57,20 +88,32 @@ int main(){
   cudaMemcpy(d_A,h_A,nBytes,cudaMemcpyHostToDevice);
   cudaMemcpy(d_B,h_B,nBytes,cudaMemcpyHostToDevice);
   cudaMemcpy(d_C,h_C,nBytes,cudaMemcpyHostToDevice);
-  // Paso 3. Realizar la suma 
-  sumaOnGPU<<<1,N>>>(d_A,d_B,d_C,N);
+  // Paso 3. Realizar la suma
+  dim3 bloque(1);
+  dim3 hilos(1024); 
+  tic = cpuTime();
+  sumaOnGPU<<<bloque,hilos>>>(d_A,d_B,d_C,N);
+  toc = cpuTime();
+  tictocP = toc - tic;
+  printf("Elapsed time for GPU: %lf segundos\n",tictocP);
   // Pass 4. 
   cudaMemcpy(h_res,d_C,nBytes,cudaMemcpyDeviceToHost);
   // Sumamos
+  tic = cpuTime();
   sumaOnCPU(h_A,h_B,h_C,N);
+  toc = cpuTime();
+  tictocS = toc - tic;
+  printf("Elapsed time for CPU: %lf segundos\n",tictocS);
   // Imprime resultado
   showVector(h_res,N);
-  printf("\n\n");
-  showVector(h_C,N);
+  compareResults(h_res,h_C,N);
+  //printf("\n\n");
+  //showVector(h_C,N);
   // Liberar memoria
   free(h_A);
   free(h_B);
   free(h_C);
+  free(h_res);
   // Paso 5. Liberar memoria
   cudaFree(d_A);
   cudaFree(d_B);
